36da49bcafee5f1f2172f58e4f7e4f0493775211
Ilya Semenov <semenovio@gmail.com>
2023-08-25 10:07:17 +0300

    Added DDL queries

--- accelerometer_landing.sql
+++ accelerometer_landing.sql
@@ -1,5 +1,17 @@
-SELECT
-    *
-FROM "accelerometer_landing"
-LIMIT 200
-
+CREATE EXTERNAL TABLE IF NOT EXISTS `stedi`.`accelerometer_landing` (
+  `user` string,
+  `timeStamp` bigint,
+  `x` double,
+  `y` double,
+  `z` double
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+  'ignore.malformed.json' = 'FALSE',
+  'dots.in.keys' = 'FALSE',
+  'case.insensitive' = 'TRUE',
+  'mapping' = 'TRUE'
+)
+STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://udacity-data-engineering-course-s3-datalake/accelerometer/landing/'
+TBLPROPERTIES ('classification' = 'json');
